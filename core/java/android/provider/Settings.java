fc17c1565a56f72c362958b5892fe893f139742d
Pawit Pornkitprasan <p.pawit@gmail.com>
2012-12-10 09:19:40 +0700

    Frameworks: Controll electronic beam animation (1/2)
    thanks Team EOS for the idea
    
    cheers
    
    Change-Id: I32e5d3d847d7d812b81d9714099fdd0387f7faca

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2915,6 +2915,16 @@ public final class Settings {
         public static final String NAVIGATION_BUTTON_GLOW_COLOR = "navigation_button_glow_color";
 
         /**
+         * @hide
+         */
+        public static final String SYSTEM_POWER_ENABLE_CRT_OFF = "system_power_enable_crt_off";
+
+        /**
+         * @hide
+         */
+        public static final String SYSTEM_POWER_ENABLE_CRT_ON = "system_power_enable_crt_on";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
