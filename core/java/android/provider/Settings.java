769ec12910acde3e1d77ef318d27fc8016c60467
Lars Greiss <kufikugel@googlemail.com>
2013-02-25 05:38:47 +0100

    Frameworks: ElectronBeam get some love (1/2)
    This commit removes the CRT On animation because it is just
    not nice implemented from AOSP and not really needed. If ppl
    turn on the screen they want to see fast their content. But still
    CRT animation needed some love. So this commit introduces another
    SlimBean mode. Crt animation mode:
    
    Features:
    - Crt Off animation controll (on/off like it was before)
    - You can select different crt off animation modes
    - always horizontal (stock behaviour)
    - always vertical (screen collapse from left and right to the middle)
    - orientation dependent (means portrait = horizontal animation, landscape = vertical animation)
    
    cheers
    
    Change-Id: I193313619e8b26f7c4a08e864310d4eaa62225f3

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2915,14 +2915,17 @@ public final class Settings {
         public static final String NAVIGATION_BUTTON_GLOW_COLOR = "navigation_button_glow_color";
 
         /**
+         * Whether electronic beam animation is enabled or not
          * @hide
          */
         public static final String SYSTEM_POWER_ENABLE_CRT_OFF = "system_power_enable_crt_off";
 
         /**
+         * Electronic beam animation mode
+         * 0 = always horizontal, 1 = always vertical, 2 = dependent on orientation
          * @hide
          */
-        public static final String SYSTEM_POWER_ENABLE_CRT_ON = "system_power_enable_crt_on";
+        public static final String SYSTEM_POWER_CRT_MODE = "system_power_crt_mode";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
