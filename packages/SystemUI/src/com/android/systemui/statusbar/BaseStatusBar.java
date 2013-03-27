e840711b37630c6eb78fe391eee927cc1ac8a136
Jens Doll <jens.doll@gmail.com>
2013-03-27 14:53:04 +0100

    Merge Navigation Bar and Pie Controls button config (1/2)
    Based on the commit series for pie controls, this commit
    makes the Navigation Bar and Pie Controls share
    the same button layout. However, there is no button layout
    editor for pie controls, which makes the navigation bar
    the leading entity when it comes to button layouts.
    
    This commit introduces:
    * A common notion of navigation buttons used in CyanogenMod.
      (class NavigationButtons)
    * Some setting constants changes to avoid clashes with PA.
    * Removal of the pie search key, since it is no longer needed.
    * Sophisticated key event injection.
    * Virtual key sound effect
    
    Change-Id: Id68f8d0b597d7d89c55c3f66f994e632d014f006
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -1640,7 +1640,7 @@ public abstract class BaseStatusBar extends SystemUI implements
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.PIE_CONTROLS), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
-                    Settings.System.PIE_GRAVITY), false, this);
+                    Settings.System.PIE_POSITIONS), false, this);
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.EXPANDED_DESKTOP_STATE), false, this);
         }
@@ -1648,7 +1648,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         @Override
         public void onChange(boolean selfChange) {
             mPieTriggerSlots = Settings.System.getInt(mContext.getContentResolver(),
-                    Settings.System.PIE_GRAVITY, Position.BOTTOM.FLAG);
+                    Settings.System.PIE_POSITIONS, Position.BOTTOM.FLAG);
 
             attachPie();
         }
