451fcea99febb78d606277cfcffb11e6c5649d54
Jens Doll <jens.doll@gmail.com>
2013-04-17 17:32:04 +0200

    Pie controls: Introducing a pie delivery service
    To make pie controls more reliable, it is neccessary to detect
    trigger actions directly from the input stream.
    This commit introduces a new system service, that filters all
    input events in front of the input dispatcher to detect pie activations.
    
    This commit introduces:
    * A new system server local API in the input manager service to register
      secondary input filters. These filters are behind the default
      accessibility filter but before the input event dispatching of
      the android framework.
    * A new system service, that binds to the new API to listen for pie
      activation gestures.
    * A non-public manager class providing access to the newly created pie
      service.
    
    The service manager name of the service is "pieservice". The non-public
    AIDL interface of the service is IPieService.aidl. To register a new
    pie activation listener the INJECT_INPUT permission is needed. The
    service state can be dumped by the "dumpsys pieservice" command.
    
    Note: This commit only introduces the pie service. There is another
    commit, that binds the actual pie controls to the pie service.
    
    Patch Set #1:
    * The pie service is currently disabled by default and needs to be
      enabled by device overlays (see config.xml / config_allowPieService).
    
    Patch Set #2:
    * Activation fixes
    * Debug dump improvements
    
    Patch Set #4:
    * Added systrace support (TRACE_INPUT_TAG)
    * Switch default to enable service on all devices.
    * Moved Position to com.internal.android.utils.pie.*
    * Some more code rearrangements
    
    Patch Set #5:
    * Rebase
    
    Patch Set #6:
    * Cover more corner cases on PieInputFilter
    * Adjust gesture time out
    
    Patch Set #7:
    * Do not send events that are from the past
    * Recycle all events
    
    Patch Set #8:
    * Handle binder died events in PieService correctly
    
    Patch Set #10:
    * Simplified locking
    * SYSTEM_UI_FLAG_HIDE_NAVIGATION support
    * Fixed ADW Lauchner bug
    
    Change-Id: I6a4a4635bed420e800a3230457ee690131116a11
    
    Conflicts:
    	core/res/res/values/dimens.xml
    	core/res/res/values/symbols.xml
    	services/java/com/android/server/SystemServer.java

--- services/java/com/android/server/SystemServer.java
+++ services/java/com/android/server/SystemServer.java
@@ -60,6 +60,7 @@ import com.android.server.input.InputManagerService;
 import com.android.server.net.NetworkPolicyManagerService;
 import com.android.server.net.NetworkStatsService;
 import com.android.server.os.SchedulingPolicyService;
+import com.android.server.pie.PieService;
 import com.android.server.pm.Installer;
 import com.android.server.pm.PackageManagerService;
 import com.android.server.pm.UserManagerService;
@@ -365,6 +366,7 @@ class ServerThread extends Thread {
         TextServicesManagerService tsms = null;
         LockSettingsService lockSettings = null;
         DreamManagerService dreamy = null;
+        PieService pieService = null;
 
         // Bring up services needed for UI.
         if (factoryTest != SystemServer.FACTORY_TEST_LOW_LEVEL) {
@@ -763,6 +765,16 @@ class ServerThread extends Thread {
                 new IdleMaintenanceService(context, battery);
             } catch (Throwable e) {
                 reportWtf("starting IdleMaintenanceService", e);
+
+            if (context.getResources().getBoolean(
+                    com.android.internal.R.bool.config_allowPieService)) {
+                try {
+                    Slog.i(TAG, "Pie Delivery Service");
+                    pieService = new PieService(context, wm, inputManager);
+                    ServiceManager.addService("pieservice", pieService);
+                } catch (Throwable e) {
+                    Slog.e(TAG, "Failure starting Pie Delivery Service Service", e);
+                }
             }
         }
 
@@ -847,6 +859,13 @@ class ServerThread extends Thread {
             reportWtf("making Display Manager Service ready", e);
         }
 
+        if (pieService != null) {
+            try {
+                pieService.systemReady();
+            } catch (Throwable e) {
+                reportWtf("making Pie Delivery Service ready", e);
+            }
+        }
 
         IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_APP_LAUNCH_FAILURE);
