28d64e7512d7a4d523ebaabafd329b49b30306a7
Veeti Paananen <veeti.paananen@rojekti.fi>
2013-08-14 17:29:00 +0300

    Remove redundant click action from date & time container layout
    Unlike AOSP, CyanogenMod provides separate actions for both the clock
    and date & time views in the expanded notification area. AOSP has a
    single action for both views.
    
    This commit removes the redundant AOSP click action completely. In
    addition, the clock and date views are modified to occupy the entire
    height of the layout to increase the clickable area.
    
    Change-Id: I78f4353c9ae4646f14124a38ef94e44ab920a067
    
    Conflicts:
    	packages/SystemUI/res/layout/status_bar_expanded_header.xml
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -433,10 +433,6 @@ public class PhoneStatusBar extends BaseStatusBar {
         mHasFlipSettings = res.getBoolean(R.bool.config_hasFlipSettingsPanel);
 
         mDateTimeView = mNotificationPanelHeader.findViewById(R.id.datetime);
-        if (mDateTimeView != null) {
-            mDateTimeView.setOnClickListener(mClockClickListener);
-            mDateTimeView.setEnabled(true);
-        }
 
         mSettingsButton = (ImageView) mStatusBarWindow.findViewById(R.id.settings_button);
         if (mSettingsButton != null) {
@@ -2330,6 +2326,7 @@ public class PhoneStatusBar extends BaseStatusBar {
         }
     };
 
+<<<<<<< HEAD
     private View.OnClickListener mHaloButtonListener = new View.OnClickListener() {
         public void onClick(View v) {
             // Activate HALO
@@ -2350,6 +2347,10 @@ public class PhoneStatusBar extends BaseStatusBar {
     };
 
     private View.OnClickListener mNotificationButtonListener = new View.OnClickListener() {
+=======
+    private final View.OnClickListener mNotificationButtonListener = new View.OnClickListener() {
+        @Override
+>>>>>>> 7916ca9... Remove redundant click action from date & time container layout
         public void onClick(View v) {
             animateExpandNotificationsPanel();
         }
