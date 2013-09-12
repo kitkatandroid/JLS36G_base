8c6a8131bc3bc4922c4a0449bd043bbdda2399fb
ayysir <dresadd09691@gmail.com>
2013-09-12 13:22:44 -0400

    Halo Mod: Double Derp

--- packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/BaseStatusBar.java
@@ -352,7 +352,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         }
     }
 
-    protected void restartHalo() {
+    public void restartHalo() {
         if (mHalo != null) {
             mHalo.cleanUp();
             mWindowManager.removeView(mHalo);
@@ -362,7 +362,7 @@ public abstract class BaseStatusBar extends SystemUI implements
         updateHalo();
     }
 
-    public void updateHalo() {
+    protected void updateHalo() {
         mHaloActive = Settings.System.getInt(mContext.getContentResolver(),
                 Settings.System.HALO_ACTIVE, 0) == 1;
 
