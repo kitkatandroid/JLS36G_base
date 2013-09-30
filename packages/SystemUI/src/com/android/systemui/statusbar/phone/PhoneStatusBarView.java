98752a06e0172b459934d40a80d2f45f2e6b7d58
Alejandro <kitkatandroid4.4trek@gmail.com>
2013-09-30 09:48:21 +0200

    Revert "Add pie control optimizations for Phone UI."
    This reverts commit 02d2658be241ca305da7a5eda177146841366770.

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBarView.java
@@ -31,7 +31,6 @@ import android.view.accessibility.AccessibilityEvent;
 
 import com.android.systemui.EventLogTags;
 import com.android.systemui.R;
-import com.android.systemui.statusbar.policy.PieController.Position;
 
 public class PhoneStatusBarView extends PanelBar {
     private static final String TAG = "PhoneStatusBarView";
@@ -173,12 +172,6 @@ public class PhoneStatusBarView extends PanelBar {
         mFadingPanel = null;
         mLastFullyOpenedPanel = null;
 
-	// show up you pie controls
-        mBar.updatePieTriggerMask(Position.LEFT.FLAG
-                | Position.TOP.FLAG
-                | Position.RIGHT.FLAG
-                | Position.TOP.FLAG);
-
         Settings.System.putInt(mContext.getContentResolver(),
             Settings.System.TOGGLE_NOTIFICATION_SHADE, 0);
     }
@@ -189,14 +182,6 @@ public class PhoneStatusBarView extends PanelBar {
         if (openPanel != mLastFullyOpenedPanel) {
             openPanel.sendAccessibilityEvent(AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED);
         }
-
-        // back off you pie controls!
-        if (mShouldFade) {
-            mBar.updatePieTriggerMask(Position.LEFT.FLAG
-                    | Position.RIGHT.FLAG
-                    | Position.TOP.FLAG);
-        }
-
         mFadingPanel = openPanel;
         mLastFullyOpenedPanel = openPanel;
         mShouldFade = true; // now you own the fade, mister
